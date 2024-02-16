d60b095a1ba58c6f61ffa7f674dd6c062f865688
DeveshKamble <d.kamble@iitg.ac.in>
2024-02-16 17:58:31 +0530

    Added Page headers component

---
+++ MUN_IITG_2024/src/pages/AboutUs/aboutUs.jsx
@@ -0,0 +1,13 @@
+import PageHeader from '../../components/PageHeader/pageHeader';
+import styles from './aboutUsStyles.module.css'
+
+const AboutUs = () => {
+    return ( 
+        <>
+            <PageHeader title={'Previous Editions'} color={'#A0EDC9'} />
+            {/* <div className={styles.titleContainer}>Hello</div> */}
+        </>
+     );
+}
+ 
+export default AboutUs;
