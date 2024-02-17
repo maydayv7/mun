7850e27f1fe2a0990f57ebfae107b0f6a3d350ec
DeveshKamble <d.kamble@iitg.ac.in>
2024-02-17 08:28:29 +0530

    Added description for about us

---
+++ MUN_IITG_2024/src/components/Letter/letter.jsx
@@ -0,0 +1,11 @@
+import styles from './letterStyles.module.css'
+
+const Letter = () => {
+    return ( 
+        <div className={styles.letterContainer}>
+            
+        </div>
+     );
+}
+ 
+export default Letter;
--- MUN_IITG_2024/src/pages/AboutUs/aboutUs.jsx
+++ MUN_IITG_2024/src/pages/AboutUs/aboutUs.jsx
@@ -1,11 +1,20 @@
+import Letter from '../../components/Letter/letter';
 import PageHeader from '../../components/PageHeader/pageHeader';
 import styles from './aboutUsStyles.module.css'
 
 const AboutUs = () => {
     return ( 
         <>
-            <PageHeader title={'Previous Editions'} color={'#A0EDC9'} />
+            <PageHeader title={'About Us'} color={'#A0EDC9'} />
             {/* <div className={styles.titleContainer}>Hello</div> */}
+            <div className={styles.description}>
+                <p className={styles.content}>
+                    The MUN, or the Model United Nations, is a simulation of the actual UN. The participants pretend to represent a nation, and come to together to discuss the very same issues being debated upon in the UN. This gives a normal person the opportunity to know what’s its like to stand for something more than himself/herself, and to experience the complicated the verbal dance and that politicians and delegates take part in everyday. The MUN is a conclave for ideation by the fresh minds of our nation, to brainstorm solutions to problems plaguing our world. Participants work through the tenuous relationships between nations as they deliberate towards a resolution – and they get to look nice in a suit and tie while they do it!
+                </p>
+            </div>
+            <div className={styles.letterContainer}>
+                <Letter/>
+            </div>
         </>
      );
 }
