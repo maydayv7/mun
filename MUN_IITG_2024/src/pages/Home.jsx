2d2a3679cc31e609a921f61ec97853d91ffd8e12
ANSULJAIN <ansuljain2003@gmail.com>
2024-03-02 12:41:43 +0530

    home page

--- MUN_IITG_2024/src/pages/Home.jsx
+++ MUN_IITG_2024/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import Navbar from '../components/general/Navbar'
 import GreenPurpleFrame from '../components/page1'
-
+import Mun from '../components/Home/page'
 export default function Home(){
 
     return(<>
   
    {/* <Navbar/> */}
-     <GreenPurpleFrame/> 
+     <Mun/> 
     
     
    
