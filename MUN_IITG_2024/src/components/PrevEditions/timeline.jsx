425b135f0bf09a3beaa20a6cc493826a7c87f079
DeveshKamble <d.kamble@iitg.ac.in>
2024-03-02 20:48:02 +0530

    Added timeline

---
+++ MUN_IITG_2024/src/components/PrevEditions/timeline.jsx
@@ -0,0 +1,32 @@
+import styles from './timeline.module.css'
+import TimelineModal from './timelineModal';
+import timelineData from '../../data/timelineData'
+import arrow from '../../assets/prevEditions/arrow.svg'
+
+const Timeline = () => {
+    
+    return ( 
+        <div className={styles.timeline}>
+                {
+                    Object.entries(timelineData).map(([year,arr])=>(
+                        <div className={styles.container} key={year}>
+                            <div className={styles.side}>
+                                <div className={styles.circle} />
+                                {/* <div className={styles.arrow}></div> */}
+                                <div className={styles.sideContent}>
+                                    <img className={styles.arrow} src={arrow} alt="" />
+                                    <p className={styles.year}>{year}</p>
+                                    <p className={styles.edition}>Edition XII</p>
+                                </div>
+                            </div>
+                            <div className={styles.modal}>
+                            <TimelineModal  year={year} arr={arr} />
+                            </div>
+                        </div>
+                    ))
+                }
+        </div>
+     );
+}
+ 
+export default Timeline;
---
+++ MUN_IITG_2024/src/components/PrevEditions/timelineModal.jsx
@@ -0,0 +1,42 @@
+import styles from './timelineModal.module.css';
+const TimelineModal = ({year,arr}) => {
+    const [name, imgLink, agenda, description, bgURL] = [...arr];
+    return ( 
+        <div className={styles.box}>
+            {
+                arr.map((arr)=>(
+                        // <TimelineComponent key={item[0]} arr={item} />
+                        <div key={arr[0]} className={styles.superContainer}>
+                            <div className={styles.container}>
+                                <div className={styles.imgContainer}>
+                                    <img className={styles.img} src={arr[1]} alt="logo" />
+                                    <h3 className={styles.title}>{arr[0]}</h3>
+                                </div>
+                                <div className={styles.verticalHR}></div>
+                                <div className={styles.contentContainer}>
+                                    <h2 className={styles.agenda}>{arr[2]}</h2>
+                                    <small className={styles.description}>{arr[3]}</small>
+                                    <a href={arr[4]} className={styles.link}><button className={styles.button}>BG Guide</button></a>
+                                </div>
+                            </div>
+                            {/* <div className={styles.line} /> */}
+                        </div>
+                        ))
+            }
+        </div>
+        // <div>
+        //     <div>
+        //         <img src={imgLink} alt="logo" />
+        //         <h1>{name}</h1>
+        //     </div>
+        //     <div>
+        //         <h2>{agenda}</h2>
+        //         <small>{description}</small>
+        //         <a href={bgURL}><button>BG Guide</button></a>
+        //     </div>
+        // </div>
+     );
+}
+
+ 
+export default TimelineModal;
