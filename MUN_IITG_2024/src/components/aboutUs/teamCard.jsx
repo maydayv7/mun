972297affc75a62216a384b87b71e128b49f8a48
botketan <ketansingh918@gmail.com>
2024-03-03 16:15:23 +0530

    Changes Made to about us

--- MUN_IITG_2024/src/components/aboutUs/teamCard.jsx
+++ MUN_IITG_2024/src/components/aboutUs/teamCard.jsx
@@ -3,10 +3,10 @@ import Details from "./details";
 const TeamCard = ({name,position,socialMedia,img}) => {
     return ( 
         <div className="lg:[&>*:nth-child(even)]:opacity-0 [&>*:nth-child(even)]:hover:opacity-100 lg:[&>*:nth-child(odd)]:grayscale [&>*:nth-child(odd)]:origin-bottom [&>*:nth-child(odd)]:transition-all [&>*:nth-child(odd)]:ease-in-out [&>*:nth-child(odd)]:delay-150 [&>*:nth-child(odd)]:duration-300 lg:[&>*:nth-child(odd)]:hover:scale-[120%] [&>*:nth-child(odd)]:hover:grayscale-0 ">
-            <div className="w-[80%] sm:w-full shadow-lineLight sm:shadow-line flex flex-col justify-center">
+            <div className="w-[80%] shadow-lineLight sm:shadow-line flex flex-col justify-center">
                 
                 <div className="w-[80%] m-auto sm:w-full flex flex-col h-full items-center justify-end ">
-                    <img className="w-[161px]" src={img} />
+                    <img className="w-[120px] md:w-[161px] lg:w-[161px]" src={img} />
                 </div>
             {/* <hr className="w-[90%] h-px " /> */}
             </div>
