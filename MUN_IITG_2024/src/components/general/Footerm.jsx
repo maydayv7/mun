fc6a171ce10542812ebb90763e6343af36ebac72
maydayv7 <maydayv7@gmail.com>
2025-01-26 18:54:30 +0530

    Create footer

--- MUN_IITG_2024/src/components/general/Footerm.jsx
+++ MUN_IITG_2024/src/components/general/Footerm.jsx
@@ -1,28 +1,65 @@
-import './footer.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import "./footer.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-import munlogo from './footermun.svg';
+import munlogo from "./munlogo.png";
 export default function Footer() {
-    return (
-        <div className="container-main2">
-            <div className="logo_1">
-                <img src={munlogo} alt="" className='munlogo2'></img>
-            </div>
-            <div className="social">
-            <a href='https://www.instagram.com/iitg_mun/' target='_blank' className='no-underline text-[black]'><i className="bi bi-instagram fs-3"></i></a>
-            <a href='https://www.linkedin.com/company/iitgmun/' target='_blank' className='no-underline text-[black]'><i className="bi bi-linkedin fs-3"></i></a>
-            <a href='https://www.facebook.com/iitgmun' target='_blank' className='no-underline text-[black]'><i className="bi bi-facebook fs-3"></i></a>
-            <a href='https://twitter.com/iitg_mun' target='_blank' className='no-underline text-[black]'><i className="bi bi-twitter-x fs-3"></i></a>
-
-            </div>
-            <p>Designed by Adi and Abdul </p>
-            <p>Developed by Ansul, Devesh and  Deepak</p>
-            <p>
-                Conference Hall, IIT Guwahati
-            </p>
-            <p>
-                Guwahati, Assam-781039
-            </p>
+  return (
+    <div className="container-main2">
+      <div className="left">
+        <div className="contact">
+          <p className="heading">Contact Us</p>
+          <p>+91 XXXXXXXXXX</p>
+          <p>+91 XXXXXXXXXX</p>
         </div>
-    );
+        <div className="address">
+          <p className="heading">Address</p>
+          <p>Conference Hall, IIT Guwahati,</p>
+          <p>North Guwahati, Guwahati,</p>
+          <p>Assam 781039</p>
+        </div>
+        <div className="mail">
+          <p className="heading">Mail Us</p>
+          <p>iitgmun@alcheringa.in</p>
+        </div>
+        <div className="social">
+          <a
+            href="https://www.instagram.com/iitg_mun/"
+            target="_blank"
+            className="no-underline text-[black]"
+          >
+            <i className="bi bi-instagram fs-3"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/company/iitgmun/"
+            target="_blank"
+            className="no-underline text-[black]"
+          >
+            <i className="bi bi-linkedin fs-3"></i>
+          </a>
+          <a
+            href="https://www.facebook.com/iitgmun"
+            target="_blank"
+            className="no-underline text-[black]"
+          >
+            <i className="bi bi-facebook fs-3"></i>
+          </a>
+          <a
+            href="https://twitter.com/iitg_mun"
+            target="_blank"
+            className="no-underline text-[black]"
+          >
+            <i className="bi bi-twitter-x fs-3"></i>
+          </a>
+        </div>
+        <div className="author">
+          <p>Developed by XYZ</p>
+          <p>Designed by XYZ</p>
+        </div>
+      </div>
+
+      <div className="logo">
+        <img src={munlogo} alt="" className="img"></img>
+      </div>
+    </div>
+  );
 }
